28bcd960fcfef5abe37e87643cddd78bfb30cb8c
ReibyArt <kushinartiom92@gmail.com>
2025-04-01 22:59:43 +0300

    Check Validating names

--- Section 3 - First Mini Project/WhoPaysTheBill/js/app.js
+++ Section 3 - First Mini Project/WhoPaysTheBill/js/app.js
@@ -18,13 +18,32 @@ const data = reactive(DEFAULT_STATE);
 // Methods //
 const addNameToList = () => {
   const userName = data.inputName;
-  // Add name in [] names
-  data.names.push(userName);
-  // Clear the input
-  data.inputName = '';
-  console.log(data.names);
-}
 
+ // Validate //
+  const validate = (value) => {
+    data.error = '';
+    if(value === ''){
+      data.error = 'Sorry! Your name is empty!'
+      return false;
+    }
+
+    if(data.names.includes(value)){
+      data.error = 'Sorry! Your names must be unique!'
+      return false; 
+    }
+    return true;
+  }
+
+  // // // //
+  if(validate(userName)) {
+    data.names.push(userName);
+    data.inputName = '';
+    data.showError = false;
+  }
+  else {
+    data.showError = true;
+  }
+}
 
 // Remove names from array
 const removeName = (index) => {
@@ -32,9 +51,9 @@ const removeName = (index) => {
 }
 
     return {
+      data,
       removeName,
       addNameToList,
-      data
     }
   }
 }).mount('#app');
