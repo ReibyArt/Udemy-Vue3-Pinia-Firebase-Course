f6e5931ea8a176ed53a1863ecb197e798cf1bffc
ReibyArt <kushinartiom92@gmail.com>
2025-04-01 23:51:45 +0300

    Project Finish -> Who pays the bill? (Who is Looser?)

--- Section 3 - First Mini Project/WhoPaysTheBill/js/app.js
+++ Section 3 - First Mini Project/WhoPaysTheBill/js/app.js
@@ -9,11 +9,17 @@ const DEFAULT_STATE = {
   result: ''
 }
 
-
 createApp({
   setup(){
 
 const data = reactive(DEFAULT_STATE);
+
+
+// Computed Properties //
+
+const isReady = computed(()=>{
+  return data.names.length > 1;
+})
 
 // Methods //
 const addNameToList = () => {
@@ -50,10 +56,53 @@ const removeName = (index) => {
   data.names.splice(index, 1);
 }
 
+
+// Random name
+const getRandomName = () => {
+  return data.names[Math.floor(Math.random() * data.names.length)];
+}
+
+const generateResult = () => {
+  let rand = getRandomName();
+
+  if(data.result !== '') {
+    while(rand === data.result){
+      rand = getRandomName();
+    }
+  }
+  data.result = rand;
+}
+
+const showResults = () => {
+  generateResult();
+    data.state = false;
+}
+
+// Reset DEFAULT_STATE 
+const resetApp = ()=> {
+  data.state = true;
+  data.input = '';
+  data.names = [];
+  data.error = '';
+  data.showError = false;
+  data.result = '';
+}
+
+// Get New Name 
+
+const getNewResult = () =>
+{
+  generateResult();
+}
+
     return {
       data,
       removeName,
       addNameToList,
+      isReady,
+      showResults,
+      resetApp,
+      getNewResult
     }
   }
 }).mount('#app');
