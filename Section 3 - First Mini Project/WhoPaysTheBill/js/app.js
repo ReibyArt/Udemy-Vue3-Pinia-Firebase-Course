21eaec3ada2c0ddc4c6b84a1cb98dde28f351ba9
ReibyArt <kushinartiom92@gmail.com>
2025-04-01 21:46:45 +0300

    Setting up the VUE app

--- Section 3 - First Mini Project/WhoPaysTheBill/js/app.js
+++ Section 3 - First Mini Project/WhoPaysTheBill/js/app.js
@@ -1,1 +1,28 @@
-console.log("Hello, Looser!");
+const { createApp, reactive, computed} = Vue;
+
+
+const DEFAULT_STATE = {
+  state: true,
+  input: '',
+  names: [],
+  error: '',
+  showError: false,
+  result: ''
+}
+
+
+createApp({
+  setup(){
+
+const data = reactive(DEFAULT_STATE);
+
+// Methods //
+const addNameToList = () => {
+
+}
+    return {
+      data,
+      addNameToList
+    }
+  }
+}).mount('#app');
