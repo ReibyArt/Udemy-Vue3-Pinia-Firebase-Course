f97d94574ade880be1b47d338af7ce4d076d6dba
ReibyArt <kushinartiom92@gmail.com>
2025-05-22 23:22:29 +0300

    Local Custom Directives || FINISH MODULE ||

--- Section 4 - Components And Nodes/src/components/Directives/index.vue
+++ Section 4 - Components And Nodes/src/components/Directives/index.vue
@@ -5,9 +5,22 @@
   <div>
     <button @click="hookText = 'Hook Text is Updated!'">Trigger UPDATE HOOKS!</button>
   </div>
+  <hr />
+  <h2>Local Directives</h2>
+  <div v-user-lastname="Lastname"></div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { onBeforeMount, ref } from 'vue'
 const hookText = ref('Hooks Text!')
+
+// Custom directive here //
+const Lastname = ref('Reiby Art Games is ref local directive!!!')
+
+const vUserLastname = {
+  beforeMount(el, binding) {
+    el.innerHTML = binding.value
+    el.style.color = 'red'
+  },
+}
 </script>
