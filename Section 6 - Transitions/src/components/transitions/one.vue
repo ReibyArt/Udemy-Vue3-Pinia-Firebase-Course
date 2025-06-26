c4af8c87a58c6e3a6587631473cf021aa4f4a5c4
ReibyArt <kushinartiom92@gmail.com>
2025-06-26 18:09:17 +0300

    56 - Named transitions

--- Section 6 - Transitions/src/components/transitions/one.vue
+++ Section 6 - Transitions/src/components/transitions/one.vue
@@ -24,6 +24,34 @@
     >
         Toggle Two
     </button>
+        <hr/>
+        <h3 class="p-2 mb-2">Named Transitions</h3>
+        <transition name="slide">
+        <div class="p-3 mb-2 bg-success text-white"
+            v-if="data.displayThree"
+        >
+            Hello Three!
+        </div>
+    </transition>
+    <button class="btn btn-primary" 
+        @click="data.displayThree = !data.displayThree"
+    >
+        Toggle Three
+    </button>
+     <hr/>
+        <h3 class="p-2 mb-2">Keyframes Transitions</h3>
+        <transition name="slideup">
+        <div class="p-3 mb-2 bg-success text-white"
+            v-if="data.displayFour"
+        >
+            Hello Four!
+        </div>
+    </transition>
+    <button class="btn btn-primary" 
+        @click="data.displayFour = !data.displayFour"
+    >
+        Toggle Three
+    </button>
 </template>
 
 <script setup>
@@ -31,7 +59,9 @@
 
    const data = reactive({
     display: false,
-    displayTwo: true  
+    displayTwo: true,
+    displayThree: true,
+    displayFour: true   
    })
 </script>
 
@@ -51,14 +81,68 @@
     opacity: 1;
 }
 
-/* Off */
-/* .v-leave-from {
+/* Named Transitions */
+
+.slide-enter-from
+{
+    transform: translate(50px);
+    opacity: 0;
+}
+.slide-enter-active {
+    transition: all 2s ease;
+}
+.slide-enter-to
+{
     opacity: 1;
 }
-.v-leave-active {
-    transition: 2s;
+
+/* leave */
+.slide-leave-from
+{
+    opacity: 1;
+    background-color: rgb(9, 255, 0) !important;
 }
-.v-leave-to {
+.slide-leave-active {
+    transition: all 2s ease;
+}
+.slide-leave-to
+{
+    background-color: rgb(247, 243, 14) !important;
+    transform: translate(200px);
     opacity: 0;
-} */
+}
+
+/* Keyframes */
+
+.slideup-enter-active {
+    animation: slideUp 1s ease-in-out;
+}
+.slideup-leave-active {
+    animation: slideDown 2s ease-in-out
+}
+
+@keyframes slideUp {
+    from {
+        opacity: 0;
+        transform: translateY(10px);
+    }
+    to {
+        opacity: 1;
+        transform: translateY(0);
+    }
+}
+
+@keyframes slideDown {
+    from {
+        opacity: 1;
+        transform: translateY(0);
+
+    }
+    to {
+         opacity: 0;
+        transform: translateY(10px);
+    }
+}
+
+
 </style>
