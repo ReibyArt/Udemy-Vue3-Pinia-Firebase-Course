58a7ad9155e6a355e96f081bf2c8bad90025057f
ReibyArt <kushinartiom92@gmail.com>
2025-06-27 00:44:57 +0300

    57. Transition mode

--- Section 6 - Transitions/src/components/transitions/two.vue
+++ Section 6 - Transitions/src/components/transitions/two.vue
@@ -1,20 +1,49 @@
 <template>
-    <div class="p-3 mb-2 bg-success text-white"
-        v-if="data.display"
-    >
-        Hello
-    </div>
-    <button class="btn btn-primary" 
-        @click="data.display = !data.display"
-    >
-        Toggle
-    </button>
+<div>
+   <button class="btn btn-primary" @click="status = !status">
+      Toggle Status
+   </button>
+</div>
+<transition mode="out-in" name="custom" enter-active-class="dog" leave-active-class="cat">
+    <!-- Альтернативный режим - in-out (сначала появляется новый элемент, потом исчезает старый), но out-in используется чаще. -->
+   <div class="p-3 mb-2 bg-danger text-white" v-if="!status" key="status_off">
+      OFF
+   </div>
+      
+   <div class="p-3 mb-2 bg-success text-white" v-else="status" key="status_on">
+      ON
+   </div>
+</transition>
+
 </template>
 
 <script setup>
-   import { reactive } from 'vue';
+   import { ref } from 'vue';
 
-   const data = reactive({
-    display:false
-   })
+
+const status = ref(false);
 </script>
+
+<style>
+
+/* On */
+.custom-enter-from,
+.custom-leave-to {
+    opacity: 0;
+}
+.custom-enter-active,
+.custom-leave-active {
+    transition: 3s;
+}
+.custom-enter-to,
+.custom-leave-from {
+    opacity: 1;
+}
+
+/* Example */
+
+.dog, .cat{
+   transition: 1s;
+}
+
+</style>
