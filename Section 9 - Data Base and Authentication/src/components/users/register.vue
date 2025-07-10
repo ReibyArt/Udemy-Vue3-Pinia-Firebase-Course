fe06a064d6be931000c28aa516142e9445ef33bc
ReibyArt <kushinartiom92@gmail.com>
2025-07-10 20:38:56 +0300

    Sign in user

--- Section 9 - Data Base and Authentication/src/components/users/register.vue
+++ Section 9 - Data Base and Authentication/src/components/users/register.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="row justify-content-md-center">
         <div class="col col-lg-5">
-            <h1>Register</h1>
+            <h1 v-text="register ? 'Register' : 'Sign in'"></h1>
             <hr/>
             <form @submit.prevent="submitForm">
                 <div class="mb-3">
@@ -17,6 +17,13 @@
                     Submit
                 </button>
             </form>
+            <hr>
+            <button 
+              class="btn btn-outline-primary"
+              v-text="register ? 'Want to sign in' : 'Want to register?'"
+              @click="register = !register"
+            />
+
         </div>
     </div>
 </template>
@@ -24,7 +31,7 @@
 
 <script setup>
   import { AUTH } from '@/firebase/config';
-  import { createUserWithEmailAndPassword } from 'firebase/auth';
+  import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
   import { reactive, ref } from 'vue';
   import { useRouter } from 'vue-router';
 
@@ -41,7 +48,23 @@
       registerUser();
     }
     else {
+      signInUser();
+    }
+  }
 
+  // Sign in User //
+
+  const signInUser = async() => {
+    try {
+      const response = await signInWithEmailAndPassword(AUTH, formData.email, formData.password);
+      if(!response){
+          throw new Error("Sorry, something happened!");
+        }
+        console.log("sign in success");
+        router.push('/'); 
+    }
+    catch(error){
+      console.log(error);
     }
   }
 
@@ -52,7 +75,8 @@
         if(!response){
           throw new Error("Sorry, something happened!");
         }
-        router.push('/'); 
+        router.push('/');
+        console.log("register success"); 
     } 
     catch(error){
       console.log(error.message);
