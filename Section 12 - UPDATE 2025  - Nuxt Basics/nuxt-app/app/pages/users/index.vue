60ac22c43508ff47e19bfc5cb83b52353dc3e02d
ReibyArt <kushinartiom92@gmail.com>
2025-10-05 23:03:37 +0300

    NUXT - Nested Routes

---
+++ Section 12 - UPDATE 2025  - Nuxt Basics/nuxt-app/app/pages/users/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>Nested Routes</h1>
+        <h1>Hello User!</h1>
+    </div>
+</template>
---
+++ Section 12 - UPDATE 2025  - Nuxt Basics/nuxt-app/app/pages/users/profile.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>Users Routes Profile</h1>
+    </div>
+</template>
