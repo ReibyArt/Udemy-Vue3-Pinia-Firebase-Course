babdf1a37c92248a93d0b05c9cb51487fe90e79e
ReibyArt <kushinartiom92@gmail.com>
2025-10-05 22:52:57 +0300

    NUXT - Static Routes

---
+++ Section 12 - UPDATE 2025  - Nuxt Basics/nuxt-app/app/pages/contact.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>Static Routes</h1>
+        <h1>Contact Page</h1>
+    </div>
+</template>
---
+++ Section 12 - UPDATE 2025  - Nuxt Basics/nuxt-app/app/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>Static Routes</h1>
+        <h1>Hello Home!</h1>
+    </div>
+</template>
