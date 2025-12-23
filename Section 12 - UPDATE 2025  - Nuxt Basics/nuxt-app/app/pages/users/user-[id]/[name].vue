fbc74de68d927456a76e36f2b97a98856ea88a1d
ReibyArt <kushinartiom92@gmail.com>
2025-12-23 23:43:33 +0300

    106. Dynamic routes

---
+++ Section 12 - UPDATE 2025  - Nuxt Basics/nuxt-app/app/pages/users/user-[id]/[name].vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <!-- Simple Example -->
+        <!-- http://localhost:3000/users/123/name -->
+         <!-- Another way -->
+         <!-- user-[id] -->
+         <!-- http://localhost:3000/users/user-123/name -->
+        User ID and name
+    </div>
+</template>
---
+++ Section 12 - UPDATE 2025  - Nuxt Basics/nuxt-app/app/pages/users/user-[id]/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <!-- Dynamic ID  -->
+    <!-- http://localhost:3000/users/123 
+        or 
+     http://localhost:3000/users/ReibyArtBlablabala -->
+    <div>User ID</div>
+</template>
