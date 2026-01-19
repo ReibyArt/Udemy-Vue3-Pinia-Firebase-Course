5fdc0640a76dbf805a4c6a76d2b03e38c80585e8
ReibyArt <kushinartiom92@gmail.com>
2026-01-19 21:09:45 +0300

    NUXT - 108. Catch all routes

---
+++ Section 12 - UPDATE 2025  - Nuxt Basics/nuxt-app/app/pages/cars/[...name].vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>Cars Page [Dynamic Routes]</h1>
+    <h2>http://localhost:3000/cars</h2>
+    <p>{{ $route.params.name }}</p>
+</template>
+
+<script setup>
+     const route = useRoute();
+     console.log(route.params.name);
+</script>
