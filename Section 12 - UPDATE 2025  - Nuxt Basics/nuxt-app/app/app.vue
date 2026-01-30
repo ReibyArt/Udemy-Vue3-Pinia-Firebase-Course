a54958755aaacc137ff5f6806f1a535587844599
ReibyArt <kushinartiom92@gmail.com>
2026-01-31 01:27:35 +0300

    NUXT - 109. SEO and meta

--- Section 12 - UPDATE 2025  - Nuxt Basics/nuxt-app/app/app.vue
+++ Section 12 - UPDATE 2025  - Nuxt Basics/nuxt-app/app/app.vue
@@ -3,3 +3,15 @@
    <nuxt-page></nuxt-page>
   </div>
 </template>
+
+
+<script setup>
+
+  // UseHead support reactivity! This is imoprtant!!!!
+  useHead({
+    title: "My Awesome App",
+    meta: [{ name: 'description', content: 'Some Content Description'}],
+    link: [{ rel: 'stylesheet', href: '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">'}]
+  })
+
+</script>
--- Section 12 - UPDATE 2025  - Nuxt Basics/nuxt-app/app/pages/contact.vue
+++ Section 12 - UPDATE 2025  - Nuxt Basics/nuxt-app/app/pages/contact.vue
@@ -4,3 +4,29 @@
         <h1>Contact Page</h1>
     </div>
 </template>
+
+<script setup>
+    // UseHead support reactivity! This is imoprtant!!!!
+
+    // const title = ref('Contact Reactivity From UseHead');
+
+    // setTimeout(() => {
+    //     title.value = 'New Contact From SetTimeout'
+    // }, 2500);
+
+    // useHead({
+    //     title: title
+    // });
+
+
+    /// META
+
+    // Is not reactive!
+
+    useSeoMeta({
+        title: 'Title From Meta!',
+        ogTitle: 'OG Title',
+        description: 'Some Description From Meta',
+        ogDescription: 'Some OG Description From Meta'
+    }) 
+</script>
