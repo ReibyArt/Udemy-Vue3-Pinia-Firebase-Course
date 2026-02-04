c683da3e8b42a8bf6fcdec08ed809f5d3e8074a1
ReibyArt <kushinartiom92@gmail.com>
2026-02-05 00:50:39 +0300

    NUXT - Page Metadata

--- Section 12 - UPDATE 2025  - Nuxt Basics/nuxt-app/app/pages/users/index.vue
+++ Section 12 - UPDATE 2025  - Nuxt Basics/nuxt-app/app/pages/users/index.vue
@@ -4,3 +4,18 @@
         <h1>Hello User!</h1>
     </div>
 </template>
+
+<script setup>
+
+    const route = useRoute();
+    console.log(route.meta);
+    useHead(route.meta);
+
+    definePageMeta({
+        title: 'Users From definePageMeta',
+        meta: [
+            {name: 'description', content: 'Some description...'}
+        ],
+        something: 'Hey!'
+    })
+</script>
