9e7ae9b425c2a1fde2ef30c9714c5fd2c71a6af5
ReibyArt <kushinartiom92@gmail.com>
2026-02-11 23:46:32 +0300

    NUXT - Links and Navigation

--- Section 12 - UPDATE 2025  - Nuxt Basics/nuxt-app/app/layouts/default.vue
+++ Section 12 - UPDATE 2025  - Nuxt Basics/nuxt-app/app/layouts/default.vue
@@ -2,8 +2,19 @@
     <div>
         <header>
             <h1> Imperial Dungeons (Default Layout)</h1>
+            <!-- Links and navigation -->
+            <nav>
+                <ul>
+                    <li>
+                        <nuxt-link to="/">Home</nuxt-link>
+                    </li>
+                    <li>
+                        <nuxt-link to="/contact">Contact</nuxt-link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
             <hr/>
             <slot></slot>
-        </header>
     </div>
 </template>
--- Section 12 - UPDATE 2025  - Nuxt Basics/nuxt-app/app/pages/index.vue
+++ Section 12 - UPDATE 2025  - Nuxt Basics/nuxt-app/app/pages/index.vue
@@ -2,5 +2,14 @@
     <div>
         <h1>Static Routes</h1>
         <h1>Hello Home!</h1>
+        <nuxt-link :to="'https://google.com'" target="_blank"> Go to google </nuxt-link>
+        <hr/>
+        <button @click="router.push('/contact')">Java Script Routing Go To Contact</button>
     </div>
 </template>
+
+<script setup>
+    const router = useRouter();
+
+
+</script>
