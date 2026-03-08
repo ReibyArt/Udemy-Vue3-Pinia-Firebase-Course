18c6437d209417d1d4c4577a3f75a9cfd29e6772
ReibyArt <kushinartiom92@gmail.com>
2026-03-08 23:01:35 +0300

    NUXT - Custom 404 Page

---
+++ Section 12 - UPDATE 2025  - Nuxt Basics/nuxt-app/app/error.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <NuxtLayout>
+        <h1>My Own Custom 404 Page</h1>
+        <h1>Status Code Of Error: {{ error.statusCode }}</h1>
+        <h1>Error Message: {{ error.message }}</h1>
+        </NuxtLayout>
+    </div>
+</template>
+
+<script setup>
+    defineProps(['error']);
+</script>
