dc3529b4bb7e0614512791eb99ec0f26695da97c
ReibyArt <kushinartiom92@gmail.com>
2026-03-17 22:39:47 +0300

    NUXT SERVER - Fetching data

--- Section 13 - NUXT - The Server Side (2026)/Nuxt Server/app/pages/index.vue
+++ Section 13 - NUXT - The Server Side (2026)/Nuxt Server/app/pages/index.vue
@@ -1,5 +1,40 @@
 <template>
     <div>
         <h1>Home Page</h1>
+        <div v-if="pending">
+            <h1>Loading.....</h1>
+        </div>
+        <div v-else-if="error">Error: {{ error.message }}</div>
+        <div v-else class="row row-cols-1 row-cols-sm-2 rows-cols-md-3 g-3">
+            <div v-for="employee in data" :key="employee.id" class="col">
+                <div class="card">
+                    <div class="card-body">
+                        <h3 class="card-title">
+                            {{ employee.fullname }}
+                        </h3>
+                        <hr>
+                        <p class="card-text"> {{ employee.position }}</p>
+                        <p class="card-text"> {{ employee.age }}</p>
+                        <nuxtLink :to="`/employees/${employee.id}`" class="btn btn-primary">
+                            Details Link
+                        </nuxtLink>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
+
+<script setup>
+
+// USING $fetch, useFetch(), useAsyncFetch();
+
+    // const data = await $fetch('http://localhost:3004/employees');
+    // console.log(data);
+
+    // const {error, pending, data} = await useFetch('http://localhost:3004/employees');
+
+    const {error, pending, data} = await useAsyncData('someData', () => {
+        return $fetch('http://localhost:3004/employees');
+    });
+</script>
